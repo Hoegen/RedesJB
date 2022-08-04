fc4f71643142d7e942080c7ea2e521d70db3f357
Hoegen <marco.caesar@hotmail.com>
2022-08-04 15:11:34 -0300

    aaaaaa

--- trabalhoCG2/src/main/java/amain/Codificador.java
+++ trabalhoCG2/src/main/java/amain/Codificador.java
@@ -10,7 +10,7 @@ import org.opencv.imgcodecs.Imgcodecs;
 public class Codificador {
 	static final int TEXT_MAX = 225;
 	
-	
+	//aaaaaaa
 
 	public static String readFromImage(String path, JProgressBar progressBar) {
 		path = relativizePath(path);
